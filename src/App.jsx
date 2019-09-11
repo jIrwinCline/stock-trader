72baf047d946031f01dc436d569cc36ebe9236d5
Student <example@example.com>
2019-09-11 16:46:07 -0700

    got dotenv working FINALLY

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+
+console.log(process.env.API_KEY)
+
+const App = () => (
+  <BrowserRouter>
+  <div></div>
+  </BrowserRouter>
+);
+
+export default App;
